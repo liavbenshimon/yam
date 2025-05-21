74c8cc09915ac0741fe739c8b80db901b95aec42
liav <liav1232111@gmail.com>
2025-05-21 10:11:35 +0300

    Fix typo in InspectionForm subtitle for improved clarity.

--- components/inspection-form.tsx
+++ components/inspection-form.tsx
@@ -527,7 +527,7 @@ export default function InspectionForm({
                 <h1 className="text-xl md:text-3xl font-bold">
                   ים ניהול ואחזקה – סיור ביקורת
                 </h1>
-                <p className="mt-2 text-[#EC8305]">מצוינות במבחן היומיומי</p>
+                <p className="mt-2 text-[#EC8305]">מצוינות במבחן היום יומי</p>
                 {/* Logout Button */}
                 {!isPrinting && (
                   <div>
